e4b7f58546e4d223351354089cec380728a49a71
IhateBeingCssGuy <turigergo14@gmail.com>
2025-06-06 15:34:11 +0200

    ewrwer
    ewrwer

--- app/Models/Eszkozok.php
+++ app/Models/Eszkozok.php
@@ -6,6 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class Eszkozok extends Model
 {
+    protected $table = 'adat_eszkozok';
     protected $fillable = [
         'device_id', 'device', 'os', 'app_version',
     ];
