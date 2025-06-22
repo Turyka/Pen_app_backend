94261aabcf6248e96c72c49b73d76e66e2b6e599
IhateBeingCssGuy <turigergo14@gmail.com>
2025-06-23 00:40:22 +0200

    test
    test

--- resources/views/dashboard/Parts/_header.blade.php
+++ resources/views/dashboard/Parts/_header.blade.php
@@ -65,7 +65,7 @@
                 </form>
 
                 <!-- Logout link -->
-                <a href="#" style="z-index: 100;" class="inline-flex items-center w-full px-2 py-1 text-sm font-semibold transition-colors duration-150 rounded-md hover:bg-gray-100 hover:text-gray-800 dark:hover:bg-gray-800 dark:hover:text-gray-200"
+                <a href="#" style="z-index: 100 !important;" class="inline-flex items-center w-full px-2 py-1 text-sm font-semibold transition-colors duration-150 rounded-md hover:bg-gray-100 hover:text-gray-800 dark:hover:bg-gray-800 dark:hover:text-gray-200"
                   onclick="event.preventDefault(); document.getElementById('logout-form').submit();">
                   <svg class="w-4 h-4 mr-3" aria-hidden="true" fill="none" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
                       viewBox="0 0 24 24" stroke="currentColor">
