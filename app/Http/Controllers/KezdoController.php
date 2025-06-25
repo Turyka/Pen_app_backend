d16f5337d01c430533447a0426be1e75a9577682
IhateBeingCssGuy <turigergo14@gmail.com>
2025-06-25 22:01:17 +0200

    ewrwe
    rew

--- app/Http/Controllers/KezdoController.php
+++ app/Http/Controllers/KezdoController.php
@@ -77,7 +77,6 @@ class KezdoController extends Controller
             'count' => $logins[$date->toDateString()] ?? 0,
         ];
     }
-    dd($napilogin);
 
     return view('dashboard.index', compact('users','naptar_szamok','eszkozok_szamok','hir_Szamok','eszkozok','napilogin'));
     }
