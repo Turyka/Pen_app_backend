bac0ae81645174c2733b995e59459ea4b73bc6d3
IhateBeingCssGuy <turigergo14@gmail.com>
2025-06-26 00:24:26 +0200

    Teljses fix
    fix

--- resources/views/dashboard/index.blade.php
+++ resources/views/dashboard/index.blade.php
@@ -2,7 +2,8 @@
 <html :class="{ 'theme-dark': dark }" x-data="data()" lang="HU">
 
 <head>
-  <title>Főmenü</title>
+  <title>Főmenü | PEN </title>
+   <!-- Script megkapja javascriptben -->
   <script>
     window.deviceChartData = @json($eszkozok);
     window.napilogin = @json($napilogin);
@@ -13,7 +14,6 @@
 <body>
   <div class="flex h-screen bg-gray-50 dark:bg-gray-900" :class="{ 'overflow-hidden': isSideMenuOpen }">
     @include('dashboard.Parts._slide')
-    <!-- Mobile sidebar -->
     <!-- Backdrop -->
     <div x-show="isSideMenuOpen" x-transition:enter="transition ease-in-out duration-150"
       x-transition:enter-start="opacity-0" x-transition:enter-end="opacity-100"
@@ -29,17 +29,18 @@
 
       @include('dashboard.Parts._slide_mobil')
     </aside>
-    <div class="flex flex-col flex-1 w-full">
 
+    <div class="flex flex-col flex-1 w-full">
+       <!-- header  -->
       @include('dashboard.Parts._header')
       <main class="h-full overflow-y-auto">
         <div class="container px-6 mx-auto grid">
           <h2 class="my-6 text-2xl font-semibold text-gray-700 dark:text-gray-200">
             Dashboard
           </h2>
-          <!-- Cards -->
           <div class="grid gap-6 mb-8 md:grid-cols-2 xl:grid-cols-4">
-            <!-- Card -->
+
+            <!-- Eszközök számok card -->
             <div class="flex items-center p-4 bg-white rounded-lg shadow-xs dark:bg-gray-800">
               <div class="p-3 mr-4 text-orange-500 bg-orange-100 rounded-full dark:text-orange-100 dark:bg-orange-500">
                 <svg class="w-6 h-6 text-gray-800 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" viewBox="0 0 24 24">
@@ -55,7 +56,8 @@
                 </p>
               </div>
             </div>
-            <!-- Card -->
+
+            <!-- Létrehozott Quiz Card -->
             <div class="flex items-center p-4 bg-white rounded-lg shadow-xs dark:bg-gray-800">
               <div class="p-3 mr-4 text-green-500 bg-green-100 rounded-full dark:text-green-100 dark:bg-green-500">
                 <svg class="w-6 h-6 text-gray-800 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
@@ -71,7 +73,8 @@
                 </p>
               </div>
             </div>
-            <!-- Card -->
+
+            <!-- HÍrek száma Card -->
             <div class="flex items-center p-4 bg-white rounded-lg shadow-xs dark:bg-gray-800">
               <div class="p-3 mr-4 text-blue-500 bg-blue-100 rounded-full dark:text-blue-100 dark:bg-blue-500">
                 <svg class="w-6 h-6 text-gray-800 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
@@ -87,7 +90,8 @@
                 </p>
               </div>
             </div>
-            <!-- Card -->
+
+            <!-- Naptár postok Card -->
             <div class="flex items-center p-4 bg-white rounded-lg shadow-xs dark:bg-gray-800">
               <div class="p-3 mr-4 text-teal-500 bg-teal-100 rounded-full dark:text-teal-100 dark:bg-teal-500">
                 <svg class="w-6 h-6 text-gray-800 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
@@ -105,45 +109,39 @@
             </div>
           </div>
 
+          <div class="w-full overflow-hidden rounded-lg shadow-xs">
+            <div class="w-full overflow-x-auto">
+              <table class="w-full whitespace-no-wrap">
+                <thead>
+                  <tr
+                    class="text-xs font-semibold tracking-wide text-left text-gray-500 uppercase border-b dark:border-gray-700 bg-gray-50 dark:text-gray-400 dark:bg-gray-800">
+                    <th class="px-4 py-3">Felhasználók</th>
+                    <th class="px-4 py-3">Szak</th>
+                    <th class="px-4 py-3">Titulus</th>
+                  </tr>
+                </thead>
+                <!-- Táblák az emberekről -->
+                @foreach ($users as $user)
+                  <x-user-card :user="$user" />
+                @endforeach
+              </table>
+            </div>
+          </div>
 
-
-          
-<div class="w-full overflow-hidden rounded-lg shadow-xs">
-  <div class="w-full overflow-x-auto">
-    <table class="w-full whitespace-no-wrap">
-      <thead>
-        <tr
-          class="text-xs font-semibold tracking-wide text-left text-gray-500 uppercase border-b dark:border-gray-700 bg-gray-50 dark:text-gray-400 dark:bg-gray-800">
-          <th class="px-4 py-3">Felhasználók</th>
-          <th class="px-4 py-3">Szak</th>
-          <th class="px-4 py-3">Titulus</th>
-        </tr>
-      </thead>
-          <!-- New Table -->
-          @foreach ($users as $user)
-          <x-user-card :user="$user" />
-          @endforeach
-
-
-        </table>
-      </div>
-    </div>
-
-          <!-- Charts -->
+          <!-- 2 Diagramm -->
           <h2 class="my-6 text-2xl font-semibold text-gray-700 dark:text-gray-200">
             Diagramok
           </h2>
+
           <div class="grid gap-6 mb-8 md:grid-cols-2">
             <div class="min-w-0 p-4 bg-white rounded-lg shadow-xs dark:bg-gray-800">
               <h4 class="mb-4 font-semibold text-gray-800 dark:text-gray-300">
                 Összes Eszköz Márkái
               </h4>
-
-
               <canvas id="pie"></canvas>
 
               <div class="flex justify-center mt-4 space-x-3 text-sm text-gray-600 dark:text-gray-400">
-                <!-- Chart legend -->
+                <!-- Márkák -->
                 <div class="flex items-center">
                   <span class="inline-block w-3 h-3 mr-1 rounded-full" style="background-color: #0694a2"></span>
                   <span>Samsung</span>
@@ -172,18 +170,15 @@
                   <span class="inline-block w-3 h-3 mr-1 rounded-full" style="background-color: #6366f1"></span>
                   <span>Többi</span>
                 </div>
-
               </div>
             </div>
-
-
+            <!-- Napi login Diagramm -->
             <div class="min-w-0 p-4 bg-white rounded-lg shadow-xs dark:bg-gray-800">
               <h4 class="mb-4 font-semibold text-gray-800 dark:text-gray-300">
-                Forgalom (Nem működik még sadge)
+                Forgalom (még tesztelni kell)
               </h4>
               <canvas id="line"></canvas>
               <div class="flex justify-center mt-4 space-x-3 text-sm text-gray-600 dark:text-gray-400">
-                <!-- Chart legend -->
               </div>
             </div>
           </div>
@@ -192,5 +187,4 @@
     </div>
   </div>
 </body>
-
 </html>
