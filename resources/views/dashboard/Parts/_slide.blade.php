d5be02d588fcca1ba97ff4f658708782f5631e27
Turi Gergo <87238018+Turyka@users.noreply.github.com>
2025-09-25 17:11:13 +0200

    Update _slide.blade.php

--- resources/views/dashboard/Parts/_slide.blade.php
+++ resources/views/dashboard/Parts/_slide.blade.php
@@ -40,6 +40,19 @@
             <span class="ml-4">Közlemény tábla</span>
           </a>
         </li>
+
+        <li class="relative px-6 py-3">
+          <span class="{{ Route::is('kepfeltoltes') ? 'absolute inset-y-0 left-0 w-1 bg-purple-600 rounded-tr-lg rounded-br-lg' : 'bsolute inset-y-0 left-0 w-1 rounded-tr-lg rounded-br-lg' }}" aria-hidden="true"></span>
+          <a class=
+            "{{ Route::is('kepfeltoltes') ? 'inline-flex items-center w-full text-sm font-semibold text-gray-800 transition-colors duration-150 hover:text-gray-800 dark:hover:text-gray-200 dark:text-gray-100' :
+            'inline-flex items-center w-full text-sm font-semibold transition-colors duration-150 hover:text-gray-800 dark:hover:text-gray-200' }}" href="{{ route('kepfeltoltes') }}">
+<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-megaphone" viewBox="0 0 16 16">
+  <path d="M14.002 13a2 2 0 0 1-2 2h-10a2 2 0 0 1-2-2V5A2 2 0 0 1 2 3a2 2 0 0 1 2-2h10a2 2 0 0 1 2 2v8a2 2 0 0 1-1.998 2M14 2H4a1 1 0 0 0-1 1h9.002a2 2 0 0 1 2 2v7A1 1 0 0 0 15 11V3a1 1 0 0 0-1-1M2.002 4a1 1 0 0 0-1 1v8l2.646-2.354a.5.5 0 0 1 .63-.062l2.66 1.773 3.71-3.71a.5.5 0 0 1 .577-.094l1.777 1.947V5a1 1 0 0 0-1-1z"/>
+</svg>
+            <span class="ml-4">Fénykép tábla</span>
+          </a>
+        </li>
+
       </ul>
       <div class="px-6 my-6">
         <button
@@ -57,6 +70,16 @@
         type="button"
         class="flex items-center justify-between w-full px-4 py-2 text-sm font-medium leading-5 text-white transition-colors duration-150 bg-purple-600 border border-transparent rounded-lg active:bg-purple-600 hover:bg-purple-700 focus:outline-none focus:shadow-outline-purple">
       📣 Közlemény Poszt készítés
+      <span class="ml-2" aria-hidden="true">></span>
+    </button>
+      </div>
+
+      <div class="px-6 my-6">
+        <button
+        onclick="window.location='{{ route('kepfeltoltes.create') }}'"
+        type="button"
+        class="flex items-center justify-between w-full px-4 py-2 text-sm font-medium leading-5 text-white transition-colors duration-150 bg-purple-600 border border-transparent rounded-lg active:bg-purple-600 hover:bg-purple-700 focus:outline-none focus:shadow-outline-purple">
+        📸 Fénykép felöltés
       <span class="ml-2" aria-hidden="true">></span>
     </button>
       </div>
