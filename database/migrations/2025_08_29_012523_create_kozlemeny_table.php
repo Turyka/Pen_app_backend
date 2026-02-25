0f69de5675dd4d64bd0c1a107008234d233db055
Turi Gergo <87238018+Turyka@users.noreply.github.com>
2026-02-25 21:23:28 +0100

    back to back

--- database/migrations/2025_08_29_012523_create_kozlemeny_table.php
+++ database/migrations/2025_08_29_012523_create_kozlemeny_table.php
@@ -16,7 +16,7 @@ return new class extends Migration
             $table->string('title');
             $table->text('description');
             $table->tinyInteger('type');
-            $table->boolean('ertesites');
+            $table->boolean('ertesites')->default(0);
             $table->string('created');
             $table->timestamps();
         });
